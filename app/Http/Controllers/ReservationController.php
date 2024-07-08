2bb14882351f8a7307185a0e40bd78c0466d5959
fapethedev <mpetrivlin@gmail.com>
2024-07-08 20:10:33 +0000

    Impl api resources method in both controllers

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function index(): Collection|array
+    {
+        return Reservation::with('transport', 'user')->get();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'transport_id' => 'required|exists:transports,id',
+            'departure_time' => 'required|date',
+            'arrival_time' => 'required|date',
+        ]);
+
+        return Reservation::create($request->all());
+    }
+
+    public function show(Reservation $reservation)
+    {
+        return $reservation->load('transport', 'user');
+    }
+
+    public function update(Request $request, Reservation $reservation): Reservation
+    {
+        $request->validate([
+            'user_id' => 'exists:users,id',
+            'transport_id' => 'exists:transports,id',
+            'departure_time' => 'date',
+            'arrival_time' => 'date',
+        ]);
+
+        $reservation->update($request->all());
+
+        return $reservation;
+    }
+
+    public function destroy(Reservation $reservation): JsonResponse
+    {
+        $reservation->delete();
+
+        return response()->json(null, 204);
+    }
 }
--- app/Http/Controllers/TransportController.php
+++ app/Http/Controllers/TransportController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transport;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class TransportController extends Controller
 {
-    //
+    public function index(): Collection
+    {
+        return Transport::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'type' => 'required|string',
+            'name' => 'required|string',
+        ]);
+
+        return Transport::create($request->all());
+    }
+
+    public function show(Transport $transport): Transport
+    {
+        return $transport;
+    }
+
+    public function update(Request $request, Transport $transport): Transport
+    {
+        $request->validate([
+            'type' => 'string',
+            'name' => 'string',
+        ]);
+
+        $transport->update($request->all());
+
+        return $transport;
+    }
+
+    public function destroy(Transport $transport): JsonResponse
+    {
+        $transport->delete();
+
+        return response()->json(null, 204);
+    }
 }
